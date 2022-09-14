db3a93c5351d44aaaf5ebcc78dd7b864688e0ac6
huifang <hui-f.gu@hotmail.com>
2022-09-14 19:01:36 +0930

    nav update

--- controllers/albums_controller.rb
+++ controllers/albums_controller.rb
@@ -1,7 +1,6 @@
 require './models/album.rb'
 get '/albums/new' do
   images = all_images()
-
   erb :'albums/new', locals: {
     images: images
   }
--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -9,5 +9,5 @@ get '/users' do
   password = params['password']
   newsletter = params['newsletter']
   create_user(first_name, last_name, email, password)
-  redirect '/'
+  redirect '/sessions/new'
 end
